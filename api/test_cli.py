a576f30bf2fe85880953e9d7a008ba6632dfe65a
AwsomeName <liuchang_hit_cs@gmail.com>
2024-04-28 00:33:38 +0800

    updat

--- api/test_cli.py
+++ api/test_cli.py
@@ -4,31 +4,37 @@ headers = {
     "Content-Type": "application/json"
 }
 
-url = "http://0.0.0.0:8081/api/v1/chat/"
-url_upl_wav_file = "http://0.0.0.0:8081/api/v1/upload_wav_file"
-url_upl_img_file = "http://0.0.0.0:8081/api/v1/upload_img_file"
+# url = "http://0.0.0.0:8081/api/v1/chat/"
+# url_upl_wav_file = "http://0.0.0.0:8081/api/v1/upload_wav_file"
+# url_upl_img_file = "http://0.0.0.0:8081/api/v1/upload_img_file"
+
+baseip = "0.0.0.0:8081"
+baseip = "i-2.gpushare.com:58070"
+url = "http://" + baseip + "/api/v1/chat/"
+url_upl_wav_file = "http://" + baseip + "/api/v1/upload_wav_file"
+url_upl_img_file = "http://" + baseip + "/api/v1/upload_img_file"
 
 # 纯文本的请求
-data = {'date_str': "2024-04-29_20:00:00", 'user_id': '100100100', 'input_str': "您好"}
-result = requests.post(url=url, json=data, headers=headers)
-print("txt req:", result.text)
-# exit()
-
-# 图片的请求
-path_file0 = "100100100@2024-04-29_20:00:00.png"
-files = {'img_file':open(path_file0,'rb')}
-result = requests.post(url=url_upl_img_file, files=files)
-print("img req:", result.text)
-
 # data = {'date_str': "2024-04-29_20:00:00", 'user_id': '100100100', 'input_str': "您好"}
 # result = requests.post(url=url, json=data, headers=headers)
 # print("txt req:", result.text)
+# exit()
 
-# 音频的请求
-path_file0 = "100100100@2024-04-29_20:00:00.wav"
-files = {'wav_file':open(path_file0,'rb')}
-result = requests.post(url=url_upl_wav_file, files=files)
-print("wav req:", result.text)
+# 图片的请求
+# path_file0 = "100100100@2024-04-29_20:00:00.png"
+# files = {'img_file':open(path_file0,'rb')}
+# result = requests.post(url=url_upl_img_file, files=files)
+# print("img req:", result.text)
+
+# data = {'date_str': "2024-04-29_20:00:00", 'user_id': '100100100', 'input_str': "图片中是谁"}
+# result = requests.post(url=url, json=data, headers=headers)
+# print("txt req:", result.text)
+
+# # 音频的请求
+# path_file0 = "100100100@2024-04-29_20:00:00.wav"
+# files = {'wav_file':open(path_file0,'rb')}
+# result = requests.post(url=url_upl_wav_file, files=files)
+# print("wav req:", result.text)
 
 
 # url = "http://192.168.31.9:9880/?text_language=zh"
